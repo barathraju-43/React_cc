0c39bfe0541d4192211035a77e46fe03f101815a
barath <barath11raju@gmail.com>
2023-06-07 23:26:09 +0530

    day0 of react

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>The Exchange HUB</h1>
+        <ul>
+          <li>Topics</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div> 
+        <p>This is just a test page to understand react</p>
+      </div>
+    </>
+  )
+}
+
+export default App
